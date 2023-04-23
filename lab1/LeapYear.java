7c276cee2dd89626433db4bb3473a6c437f62916
yliu-fe <liuyibuaa08@gmail.com>
2023-04-23 21:47:25 +0800

    Update lab00

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,18 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year) {
+        if (year % 4 != 0){
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else if (year % 400 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
